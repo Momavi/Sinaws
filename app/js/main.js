e198543bf5d6b80870a655fa65008c9b6ff3a598
Momavi <68790545+Momavi@users.noreply.github.com>
2021-06-08 15:52:51 +0300

    1.1

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+
+  $('.webinar__slider').slick({
+    centerMode: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.menu-btn').on('click', function() {
+    $('.menu-btn').toggleClass('menu-btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+})
+
+$('.menu__list-link').on('click', function() {
+    $('.menu-btn').removeClass('menu-btn--active');
+    $('.menu__list').removeClass('menu__list--active');
+})
+});
